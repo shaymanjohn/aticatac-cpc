dd655dce57ba7f451b623dab7d44d831a4055fea
John Ward <johnmartinward@gmail.com>
2020-06-09 14:42:46 +0100

    fix up shield

--- bank_includes.asm
+++ bank_includes.asm
@@ -1,12 +1,12 @@
 org 0x4000                  ; "sprites.bin" = sprite_bank
 include "graphics/player_knight_gfx_masked.asm"
 
-org 0x4000                  ; "items.bin" = item_bank
-include "data/items.asm"
-include "graphics/item_gfx.asm"
-
 org 0x4000                ; "rooms.bin" = room_bank
 include "data/rooms.asm"
 include "data/item_list.asm"
 include "data/items_per_room.asm"
 include "data/item_pointers.asm"
+
+org 0x4000                  ; "items.bin" = item_bank
+include "data/items.asm"
+include "graphics/item_gfx.asm"
